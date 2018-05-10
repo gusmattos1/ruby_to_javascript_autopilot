cd8030567a6ec89c7d6445963a6222ef5dcf811f
gusmattos1 <gusmattos1@gmail.com>
2018-05-09 21:37:03 -0400

    assignment done

---
+++ ruby-to-js.js
@@ -0,0 +1,69 @@
+var apples = 14
+
+console.log(apples);
+
+console.log('I have ' + apples + ' apples');
+
+var materials = ['wood', 'metal', 'stone']
+
+var words = {
+              'elephant': 'The world\'s largest land mammal.',
+              'school': 'A place of learning.',
+              'ice cream': 'A delicious milk-based dessert.',
+}
+
+console.log(materials);
+console.log(words);
+
+var num = 9;
+
+if (num > 10){
+console.log(num + ' is greater than 10');}
+else if (num == 10){
+console.log(num + ' is exactly 10.');}
+else{
+console.log(num + ' must be less than 10.');}
+
+for (var i = 0; i < 10; i++) {
+  console.log('Doing the same thing over and over');
+}
+
+var base = 5;
+
+for (var i = 0; i < 20; i++){
+  console.log(base + i);
+}
+
+var total = 0
+
+for (var i = 0; i<100; i++){
+total += i;
+}
+console.log(total);
+
+for (var i = 3; i < 15; i++) {
+  if (i > 9){
+    console.log('You can get on the rollercoaster!' + i);
+  } else {
+    console.log('You are too short to ride this rollercoaster' + i);
+  }
+}
+
+var containers = ['purse', 'wallet', 'backback']
+for (var i = 0; i < containers.length; i++) {
+console.log(  containers[i]
+);}
+
+function hello_world() {
+  console.log('Hello world!');
+}
+
+hello_world()
+
+
+function add(first_num, second_num) {
+  return first_num + second_num
+}
+
+var amount = add(5, 7)
+console.log(amount);
